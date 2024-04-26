3183ed0aad7c9f73f6a30b919da5e0512f3da3d3
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-26 02:34:17 -0700

    Added operate function

--- javascipt.js
+++ javascipt.js
@@ -5,17 +5,29 @@ let operator;
 let secondNumber = 5;
 
 function add() {
-  return firstNumber + secondNumber;
+    return firstNumber + secondNumber;
 }
 
 function subtract() {
-  return firstNumber - secondNumber;
+    return firstNumber - secondNumber;
 }
 
 function multiply() {
-  return firstNumber * secondNumber;
+    return firstNumber * secondNumber;
 }
 
 function divide() {
-  return firstNumber / secondNumber;
+    return firstNumber / secondNumber;
+}
+
+function operate() {
+    if (operator === "add") {
+        add();
+    } else if (operator === "sub") {
+        subtract();
+    } else if (operator === "mult") {
+        multiply();
+    } else if (operator === "div") {
+        divide();
+    }
 }
