c7e0742bc9f1fa006f451ae4b1a89ca302cb9bde
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-26 02:29:58 -0700

    Added functions for basic math

--- javascipt.js
+++ javascipt.js
@@ -1,2 +1,21 @@
 "use strict";
 
+let firstNumber = 5;
+let operator;
+let secondNumber = 5;
+
+function add() {
+  return firstNumber + secondNumber;
+}
+
+function subtract() {
+  return firstNumber - secondNumber;
+}
+
+function multiply() {
+  return firstNumber * secondNumber;
+}
+
+function divide() {
+  return firstNumber / secondNumber;
+}
