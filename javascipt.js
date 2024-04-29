36ff1ff98d2ae7650f9edd46f0c3409c5984ea56
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-29 00:05:48 -0700

    Query selectors refactor Changed to switch statement utilizing forEach to assign an action to each press

--- javascipt.js
+++ javascipt.js
@@ -3,24 +3,68 @@
 // Query selectors for buttons
 let selector = document.querySelectorAll(".topButton, .button, .lowerButton");
 
-let clearQS = selector[0];
-let negativeQS = selector[1];
-let divideQS = selector[2];
-let sevenQS = selector[3];
-let eightQS = selector[4];
-let nineQS = selector[5];
-let multiplyQS = selector[6];
-let fourQS = selector[7];
-let fiveQS = selector[8];
-let sixQS = selector[9];
-let minusQS = selector[10];
-let oneQS = selector[11];
-let twoQS = selector[12];
-let threeQS = selector[13];
-let plusQS = selector[14];
-let zeroQS = selector[15];
-let decimalQS = selector[16];
-let equalsQS = selector[17];
+selector.forEach((selector) => {
+    selector.addEventListener("click", function () {
+        const action = selector.dataset.action;
+        console.log(action);
+        switch (action) {
+            case "clear":
+                // Code!
+                break;
+            case "negative":
+                // Code!
+                break;
+            case "divide":
+                // Code!
+                break;
+            case "seven":
+                // Code!
+                break;
+            case "eight":
+                // Code!
+                break;
+            case "nine":
+                // Code!
+                break;
+            case "multiply":
+                // Code!
+                break;
+            case "four":
+                // Code!
+                break;
+            case "five":
+                // Code!
+                break;
+            case "six":
+                // Code!
+                break;
+            case "minus":
+                // Code!
+                break;
+            case "one":
+                // Code!
+                break;
+            case "two":
+                // Code!
+                break;
+            case "three":
+                // Code!
+                break;
+            case "plus":
+                // Code!
+                break;
+            case "zero":
+                // Code!
+                break;
+            case "decimal":
+                // Code!
+                break;
+            case "equals":
+                // Code!
+                break;
+        }
+    });
+});
 
 let firstNumber = 5;
 let operator;
