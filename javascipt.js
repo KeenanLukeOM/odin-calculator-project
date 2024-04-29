a7fbcfd92834dfb50e65d625ff1f014b86c4b69b
KeenanLukeOM <76129769+KeenanLukeOM@users.noreply.github.com>
2024-04-28 23:39:43 -0700

    Query selectors

--- javascipt.js
+++ javascipt.js
@@ -1,7 +1,26 @@
 "use strict";
 
-let clear = document.querySelector(".clear");
-let negative = document.querySelector(".negative");
+// Query selectors for buttons
+let selector = document.querySelectorAll(".topButton, .button, .lowerButton");
+
+let clearQS = selector[0];
+let negativeQS = selector[1];
+let divideQS = selector[2];
+let sevenQS = selector[3];
+let eightQS = selector[4];
+let nineQS = selector[5];
+let multiplyQS = selector[6];
+let fourQS = selector[7];
+let fiveQS = selector[8];
+let sixQS = selector[9];
+let minusQS = selector[10];
+let oneQS = selector[11];
+let twoQS = selector[12];
+let threeQS = selector[13];
+let plusQS = selector[14];
+let zeroQS = selector[15];
+let decimalQS = selector[16];
+let equalsQS = selector[17];
 
 let firstNumber = 5;
 let operator;
